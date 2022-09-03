65c7b087d8b16b762b8b088b0e803b284b570930
Selim Mohamed <slowmoe329@gmail.com>
2022-09-03 14:07:44 +0200

    nn

--- sha2a/urls.py
+++ sha2a/urls.py
@@ -2,17 +2,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from Property.views import PropertyCreateView,PropertyListView,PropertyDetailView,FavoritesView
 
-
-
+admin.site.site_header = "Sakeny Admin Panel"
+admin.site.site_title = "Sakeny Admin Panel"
+admin.site.index_title = "Sakeny Admin Panel"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/users/', include('users.urls')),
-    path('api/v1/shuttle/', include('shuttle.urls')),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("user/", include("users.urls")),
+    path("property/", PropertyCreateView.as_view()),
+    path("property/all/", PropertyListView.as_view()),
+    path("property/<int:pk>/", PropertyDetailView.as_view()),
+    path("favorites/", FavoritesView.as_view()),
 
 ]
-
 urlpatterns += static(settings.STATIC_URL,
 document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
