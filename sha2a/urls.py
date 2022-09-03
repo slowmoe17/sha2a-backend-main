aa4b3e0f304ec5b87614bf19eed858e78de61ae8
Selim Mohamed <slowmoe329@gmail.com>
2022-09-03 14:05:16 +0200

    kiro url

--- sha2a/urls.py
+++ sha2a/urls.py
@@ -2,22 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from Property.views import PropertyCreateView,PropertyListView,PropertyDetailView,FavoritesView
 
-admin.site.site_header = "Sakeny Admin Panel"
-admin.site.site_title = "Sakeny Admin Panel"
-admin.site.index_title = "Sakeny Admin Panel"
+
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
-    path("user/", include("users.urls")),
-    path("property/", PropertyCreateView.as_view()),
-    path("property/all/", PropertyListView.as_view()),
-    path("property/<int:pk>/", PropertyDetailView.as_view()),
-    path("favorites/", FavoritesView.as_view()),
+    path('admin/', admin.site.urls),
+    path('api/v1/users/', include('users.urls')),
+    path('api/v1/shuttle/', include('shuttle.urls')),
 
 ]
+
 urlpatterns += static(settings.STATIC_URL,
 document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
